20006491465e2108ef2450d2a8d6758478475b44
Linus Heckemann <git@sphalerite.org>
2019-05-16 12:01:02 +0200

    Add updateBoard

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
-import Data.List (intercalate)
+import Data.List (intercalate, transpose, findIndex)
+import Data.Maybe (isNothing)
 
 data Piece = Red | Yellow deriving Show
 
@@ -7,7 +8,7 @@ type BoardLine = (Maybe Piece, Maybe Piece, Maybe Piece, Maybe Piece, Maybe Piec
 
 type BoardMatrix = (BoardLine, BoardLine, BoardLine, BoardLine, BoardLine, BoardLine, BoardLine)
 
-data BoardIndex = One | Two | Three | Four | Five | Six | Seven
+data BoardIndex = One | Two | Three | Four | Five | Six | Seven deriving (Show, Enum)
 
 type BoardPosition = (BoardIndex, BoardIndex)
 
@@ -19,6 +20,23 @@ at Four  (_, _, _, a, _, _, _) = a
 at Five  (_, _, _, _, a, _, _) = a
 at Six   (_, _, _, _, _, a, _) = a
 at Seven (_, _, _, _, _, _, a) = a
+
+boardIndices :: [BoardIndex]
+boardIndices = [One .. Seven]
+boardIndex :: Int -> BoardIndex
+boardIndex = (boardIndices !!)
+
+updateTuple :: BoardIndex -> t -> (t, t, t, t, t, t, t) -> (t, t, t, t, t, t, t)
+updateTuple One   n (a, b, c, d, e, f, z) = (n, b, c, d, e, f, z)
+updateTuple Two   n (a, b, c, d, e, f, z) = (a, n, c, d, e, f, z)
+updateTuple Three n (a, b, c, d, e, f, z) = (a, b, n, d, e, f, z)
+updateTuple Four  n (a, b, c, d, e, f, z) = (a, b, c, n, e, f, z)
+updateTuple Five  n (a, b, c, d, e, f, z) = (a, b, c, d, n, f, z)
+updateTuple Six   n (a, b, c, d, e, f, z) = (a, b, c, d, e, n, z)
+updateTuple Seven n (a, b, c, d, e, f, z) = (a, b, c, d, e, f, n)
+
+updateBoard :: BoardPosition -> Maybe Piece -> BoardMatrix -> BoardMatrix
+updateBoard (x, y) piece board = updateTuple x (updateTuple y piece (at y board)) board
 
 boardIndexToInt :: BoardIndex -> Int
 boardIndexToInt ix = at ix (1, 2, 3, 4, 5, 6, 7)
