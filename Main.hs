c75e28c6cd391ba826c57d871578fc94e3266437
Linus Heckemann <git@sphalerite.org>
2019-05-16 11:45:07 +0200

    Add tupleToList, emptyBoard

--- Main.hs
+++ Main.hs
@@ -6,6 +6,14 @@ type BoardLine = (Maybe Piece, Maybe Piece, Maybe Piece, Maybe Piece, Maybe Piec
 
 type BoardMatrix = (BoardLine, BoardLine, BoardLine, BoardLine, BoardLine, BoardLine, BoardLine)
 
+tupleToList :: (t, t, t, t, t, t, t) -> [t]
+tupleToList (a, b, c, d, e, f, g) = [ a, b, c, d, e, f, g ]
+
+emptyLine :: BoardLine
+emptyLine = (Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing)
+
+emptyBoard :: BoardMatrix
+emptyBoard = (emptyLine, emptyLine, emptyLine, emptyLine, emptyLine, emptyLine, emptyLine)
 
 main :: IO ()
 main = putStrLn "Hello, world!"
