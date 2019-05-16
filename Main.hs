9da75f165303672487038ab46c082a5bf01bd325
Linus Heckemann <git@sphalerite.org>
2019-05-16 11:29:29 +0200

    Define BoardLine and BoardMatrix types

--- Main.hs
+++ Main.hs
@@ -2,6 +2,9 @@ module Main where
 
 data Piece = Red | Yellow deriving Show
 
+type BoardLine = (Maybe Piece, Maybe Piece, Maybe Piece, Maybe Piece, Maybe Piece, Maybe Piece, Maybe Piece)
+
+type BoardMatrix = (BoardLine, BoardLine, BoardLine, BoardLine, BoardLine, BoardLine, BoardLine)
 
 
 main :: IO ()
